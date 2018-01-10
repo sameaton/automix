a67a77e37f10f5f0dd7a8c74629bb4102b7fba04
Sam Eaton <sam@fqdn.net>
2018-01-09 21:38:18 -0800

    Add command line options and debug logging

--- automix.py
+++ automix.py
@@ -2,6 +2,7 @@ import click
 import numpy as np
 import soundfile as sf
 import os.path
+import logging
 from scipy import signal
 
 def window_rms(data, window_size):
@@ -22,14 +23,29 @@ def ms_to_samples(ms, samplerate):
     return samplerate // 1000 * ms
 
 @click.command()
-@click.argument('files', nargs=2, type=click.Path(readable=True))
-def automix(files):
+@click.argument('files', nargs=2, type=click.Path(exists=True, dir_okay=False, readable=True))
+@click.option('--window', default=400, help='Window size in ms to smooth volume changes over (default 400ms)')
+@click.option('--output', default='.', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True), help='Path to output automixed files (default CWD)')
+@click.option('--verbose', is_flag=True, help='Show verbose progress')
+def automix(files, window, output, verbose):
+    """Automix takes two input mono wav files, and automatically mixes them together,
+    lowering the volume of the non-speaking channel when the other is speaking to
+    reduce bleed-through and comb-filtering.
+
+    Three output files will be produced - an automixed file, and 2 files each containing
+    the isolated channel but with the bleed attenuated"""
+
+    if verbose:
+        logging.basicConfig(style='{', format='{message}', level=logging.DEBUG)
+
     chan1, chan1_rate = sf.read(files[0])
     chan2, chan2_rate = sf.read(files[1])
     names = [os.path.splitext(os.path.basename(file))[0] for file in files]
 
-    chan1_win_rms = window_rms(chan1, ms_to_samples(400, chan1_rate))
-    chan2_win_rms = window_rms(chan2, ms_to_samples(400, chan2_rate))
+    logging.debug(f"Processing {names[0]} and {names[1]}")
+
+    chan1_win_rms = window_rms(chan1, ms_to_samples(window, chan1_rate))
+    chan2_win_rms = window_rms(chan2, ms_to_samples(window, chan2_rate))
 
     combined_win_rms = chan1_win_rms + chan2_win_rms
 
@@ -38,6 +54,10 @@ def automix(files):
 
     mixed = chan1_attenuated + chan2_attenuated
 
-    sf.write(f"automixed-{names[0]}-{names[1]}.wav", mixed, chan1_rate, 'FLOAT')
-    sf.write(f"attenuated-{names[0]}.wav", chan1_attenuated, chan1_rate, 'FLOAT')
-    sf.write(f"attenuated-{names[1]}.wav", chan2_attenuated, chan2_rate, 'FLOAT')
+    logging.debug(f"Writing output files in directory \"{output}\"")
+
+    sf.write(f"{output}/automixed-{names[0]}-{names[1]}.wav", mixed, chan1_rate, 'FLOAT')
+    sf.write(f"{output}/attenuated-{names[0]}.wav", chan1_attenuated, chan1_rate, 'FLOAT')
+    sf.write(f"{output}/attenuated-{names[1]}.wav", chan2_attenuated, chan2_rate, 'FLOAT')
+
+    logging.debug("Done")
